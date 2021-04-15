b95e147654fedb0ac9fcb419d287d38fc29d394e
aaaona <aaaona.morning@gmail.com>
2021-04-15 19:16:45 +0900

    update: App.vueとその配下のコンポーネントを初期用に調整

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./../assets/logo.png">
+    <h1>{{ title }}</h1>
+    <div>
+      <router-link to="/">Home</router-link>
+    </div>
+    <div>
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Header extends Vue {
+  @Prop()
+  private title: string | undefined;
+}
+</script>
+
+<style scoped>
+</style>
