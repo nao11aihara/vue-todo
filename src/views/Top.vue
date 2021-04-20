0273dce768b4e0b798990fc27a63c3bbc6fc02b2
aaaona <aaaona.morning@gmail.com>
2021-04-21 01:54:44 +0900

    add: 検索フォームコンポーネントを追加

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -1,15 +1,20 @@
 <template>
-  <p>トップ画面</p>
+  <v-main>
+    <SearchForm />
+  </v-main>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import SearchForm from "./../components/SearchForm.vue";
 
 /**
  * トップ画面
  */
 @Component({
-  components: {},
+  components: {
+    SearchForm,
+  },
 })
 export default class Top extends Vue {}
 </script>
