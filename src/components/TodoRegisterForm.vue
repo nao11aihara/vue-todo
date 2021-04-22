551eeaed42e74c66b2f17b943972da934c8a91d6
aaaona <aaaona.morning@gmail.com>
2021-04-22 18:03:45 +0900

    add: TODO登録フォームのバリデーションロジックを追加

--- src/components/TodoRegisterForm.vue
+++ src/components/TodoRegisterForm.vue
@@ -1,7 +1,14 @@
 <template>
   <v-card>
-    <v-text-field outlined dense label="タイトル"></v-text-field>
-    <v-btn>登録</v-btn>
+    <v-text-field
+      outlined
+      dense
+      label="タイトル"
+      v-model="inputTitle"
+      :error="isErrorTitle"
+      :error-messages="errorMessage"
+    ></v-text-field>
+    <v-btn :disabled="isDisabled">登録</v-btn>
   </v-card>
 </template>
 
@@ -12,7 +19,45 @@ import { Component, Vue } from "vue-property-decorator";
  * TODO登録フォーム
  */
 @Component
-export default class TodoRegisterForm extends Vue {}
+export default class TodoRegisterForm extends Vue {
+  // ---data---
+  /**
+   * タイトル入力値
+   */
+  public inputTitle = "";
+
+  /**
+   * エラーメッセージ
+   */
+  public errorMessage = "";
+
+  // ---computed---
+  /**
+   *  タイトルのエラー判定
+   *  @returns エラー判定
+   */
+  public get isErrorTitle(): boolean {
+    // タイトル30文字以上はバリデーションエラー
+    if (this.inputTitle.length > 30) {
+      this.errorMessage = "タイトルは30文字以内で入力してください。";
+      return true;
+    }
+    this.errorMessage = "";
+    return false;
+  }
+
+  /**
+   * ボタンが非活性状態か
+   * @returns 非活性状態判定
+   */
+  public get isDisabled(): boolean {
+    // タイトル入力値が未入力、またはエラー判定がある場合はボタンを非活性状態にする
+    if (!this.inputTitle || this.isErrorTitle) {
+      return true;
+    }
+    return false;
+  }
+}
 </script>
 
 <style scoped>
