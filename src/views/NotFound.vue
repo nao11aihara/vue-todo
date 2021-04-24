370a76408449daf6a9053ca82b54533d70c9221c
aaaona <aaaona.morning@gmail.com>
2021-04-24 23:58:03 +0900

    update: 404ページコンポーネントを修正

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,3 +1,21 @@
 <template>
-  <p>404 Not found</p>
+  <v-card>
+    <v-card-title>404 Not found</v-card-title>
+    <v-card-subtitle>お探しのページは見つかりません。</v-card-subtitle>
+  </v-card>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+/**
+ * 404ページ
+ */
+@Component
+export default class Notfound extends Vue {}
+</script>
+
+<style scoped>
+.v-card {
+  margin-top: 60px;
+}
+</style>
