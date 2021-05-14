5667dd850ab5e0bafc645f83a392ba09369eb64a
aaaona <aaaona.morning@gmail.com>
2021-05-14 19:33:24 +0900

    add: storeにステート、ゲッター、ミューテーション、アクションを追加

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,91 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import { RegisterTodoPayload } from "@/types/RegisterTodoPayload";
+import Vue from "vue";
+import Vuex, { Store } from "vuex";
+import { Todo } from "./../types/Todo";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export const store = new Vuex.Store({
-  state: {
+/**
+ * ステート型
+ */
+type State = {
+  todos: Todo[];
+  sequence: number;
+};
+
+/**
+ * ステート
+ */
+const state: State = {
+  todos: [],
+  sequence: 1,
+};
+
+/**
+ * ゲッター
+ * ステートを加工した値を取得する
+ */
+const getters = {
+  /**
+   * TODO全件取得
+   * @param state
+   * @returns TODO全件
+   */
+  getAllTodos(state: State): Todo[] {
+    return state.todos;
   },
-  mutations: {
+  /**
+   * 指定IDのTODO1件を取得
+   * @param state
+   * @param id
+   * @returns 指定IDのTODO1件
+   */
+  getTodoById(state: State, id: number): Todo | undefined {
+    return state.todos.find((todo) => todo.id === id);
   },
-  actions: {
+};
+
+/**
+ * ミューテーション
+ * ステートを更新する
+ * ※基本的にコンポーネントから呼ばない、ミューテーションはアクションからのみ呼び出すようにする
+ */
+const mutations = {
+  /**
+   * TODOを登録する
+   * @param state
+   * @param todo
+   */
+  registerTodo(state: State, todo: Todo): void {
+    state.todos.push(todo);
+    state.sequence++;
   },
-  modules: {
-  }
-})
+};
+
+/**
+ * アクション
+ * コンポーネントから直接呼び出され、外部APIとの非同期通信等を行う
+ */
+const actions = {
+  /**
+   * TODOを登録する
+   * @param commit
+   * @param state
+   * @param payload
+   */
+  registerTodo({ commit, state }: any, payload: RegisterTodoPayload): void {
+    const todo: Todo = {
+      id: state.sequence,
+      title: payload.title,
+      isDone: false,
+    };
+    commit("registerTodo", todo);
+  },
+};
+
+export const store: Store<State> = new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions,
+});
